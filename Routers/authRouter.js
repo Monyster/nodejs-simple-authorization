5436f082c5f428aa9436524cb2fc5c44b609b1c5
Matvey Voznosimenko <main.monyster@gmail.com>
2022-07-30 09:56:52 +0300

    commit #6

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -1,17 +1,25 @@
-const Router = require('express')
+const Router = require("express");
 const router = new Router();
-const controller = require('../Controllers/authController')
-const { check } = require('express-validator')
+const controller = require("../Controllers/authController");
+const { check } = require("express-validator");
+const authMiddleware = require("../Middlewares/authMiddleware");
+const roleMiddleware = require("../Middlewares/roleMiddleware");
 
-router.post('/registration', [
-    check('username', 'Field username cant be empty').notEmpty(),
-    check('password', 'Password >4 and <16').isLength({ min: 4, max: 16 })
-], controller.registration)
+router.post(
+  "/registration",
+  [
+    check("username", "Field username cant be empty").notEmpty(),
+    check("password", "Password >4 and <16").isLength({ min: 4, max: 16 }),
+  ],
+  controller.registration
+);
 
-router.post('/login', [
-    check('username', 'Field username cant be empty').notEmpty()
-], controller.login)
+router.post(
+  "/login",
+  [check("username", "Field username cant be empty").notEmpty()],
+  controller.login
+);
 
-router.get('/users', controller.getUsers)
+router.get("/users", roleMiddleware(["ADMIN"]), controller.getUsers);
 
-module.exports = router
+module.exports = router;
